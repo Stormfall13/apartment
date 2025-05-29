d3cc60da1a3a7176596e550387bd4516d36a21b0
Stormfall13 <85984394+Stormfall13@users.noreply.github.com>
2025-05-29 08:07:48 +0500

    for dev

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+
+
+
     return (
-        <footer>
-            
+        <footer className='footer'>
+            <div className="footer__wrapp">
+                <div className="column__first">
+                    <Link to="/">
+                        LOGO
+                    </Link>
+                </div>
+                <nav className='menu__column-second'>
+                    <h3 className='title'>НАШИ УСЛУГИ</h3>
+                </nav>
+            </div>
         </footer>
     )
 }
